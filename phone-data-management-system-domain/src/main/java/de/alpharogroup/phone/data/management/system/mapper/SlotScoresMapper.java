4d314df36f6820214f16227ceaba7cf605c644f0
astrapi69 <asterios.raptis@gmx.net>
2015-12-12 14:12:23 +0100

    Mapper updated after refactoring.

--- phone-data-management-system-domain/src/main/java/de/alpharogroup/phone/data/management/system/mapper/SlotScoresMapper.java
+++ phone-data-management-system-domain/src/main/java/de/alpharogroup/phone/data/management/system/mapper/SlotScoresMapper.java
@@ -2,12 +2,12 @@ package de.alpharogroup.phone.data.management.system.mapper;
 
 import org.springframework.stereotype.Component;
 
-import de.alpharogroup.db.entitymapper.AbstractEntityBOMapper;
+import de.alpharogroup.db.entitymapper.AbstractEntityDOMapper;
 import de.alpharogroup.phone.data.management.system.domain.SlotScore;
 import de.alpharogroup.phone.data.management.system.entities.SlotScores;
 
 @Component
-public class SlotScoresMapper extends AbstractEntityBOMapper<SlotScores, SlotScore>
+public class SlotScoresMapper extends AbstractEntityDOMapper<SlotScores, SlotScore>
 {
 
 }
