5ab28649a288e1114aa64c97b4d0d87048a326e6
astrapi69 <asterios.raptis@gmx.net>
2016-01-07 17:59:51 +0100

    hql statements adapted to new entity class names

--- phone-data-management-system-business/src/main/java/de/alpharogroup/phone/data/management/system/service/util/HqlStringCreator.java
+++ phone-data-management-system-business/src/main/java/de/alpharogroup/phone/data/management/system/service/util/HqlStringCreator.java
@@ -1,10 +1,12 @@
 package de.alpharogroup.phone.data.management.system.service.util;
 
+import de.alpharogroup.phone.data.management.system.entities.SlotScores;
+
 public class HqlStringCreator {
 
 	public static String forSlotScore(final String phoneNumber, final Integer score){
 		final StringBuilder sb = new StringBuilder();
-		sb.append("select ss from SlotScores ss");
+		sb.append("select ss from " + SlotScores.class.getSimpleName() + " ss");
 		final boolean phoneNumberIsNotNull = phoneNumber != null && !phoneNumber.isEmpty();
 		if (phoneNumberIsNotNull) {
 			sb.append(" ");
