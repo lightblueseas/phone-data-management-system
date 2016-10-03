534818ae547717ba38e4987fee4928db878696e4
Asterios Raptis <asterios.raptis@gmx.net>
2016-10-03 20:12:30 +0200

    update of project path in the run script

--- phone-data-management-system-rest-web/runJetty.sh
+++ phone-data-management-system-rest-web/runJetty.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export MAVEN_OPTS="-Xmx4096m -Xms4096m"
-cd /root/git/phone-data-management-system/phone-data-management-system/phone-data-management-system-rest-web
+cd /root/git/phone-data-management-system/phone-data-management-system-rest-web
 echo $MAVEN_OPTS;
 mvn exec:java -Dexec.mainClass="de.alpharogroup.phone.data.management.system.ApplicationJettyRunner"
